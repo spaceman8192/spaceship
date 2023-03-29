fcbc7c6ca68c1b7a268db202ed3678f58c1dc23e
Boyoung <bobo920524@gmail.com>
2023-03-29 21:57:37 +0900

    add mdx

--- components/common/SideMenu.js
+++ components/common/SideMenu.js
@@ -4,9 +4,9 @@ import {
   MenuItem,
   SubMenu,
   useProSidebar,
-} from "react-pro-sidebar";
+} from 'react-pro-sidebar';
 
-import styles from "@/styles/Common.module.css";
+import styles from '@/styles/Common.module.css';
 
 const SideMenu = () => {
   const { collapseSidebar } = useProSidebar();
@@ -16,12 +16,20 @@ const SideMenu = () => {
         iconShape="square"
         menuItemStyles={{
           button: ({ level, active, disabled }) => {
-            if (level === 1) return { color: active ? "#999999" : "#222222" };
+            if (level === 1) return { color: active ? '#999999' : '#222222' };
           },
         }}
       >
         <MenuItem onClick={() => collapseSidebar()}>접기/펼치기</MenuItem>
         <SubMenu label="JavaScript">
+          <MenuItem className={styles.menuItem}>Component 1</MenuItem>
+          <MenuItem className={styles.menuItem}>Component 2</MenuItem>
+        </SubMenu>
+        <SubMenu label="React.js">
+          <MenuItem className={styles.menuItem}>Component 1</MenuItem>
+          <MenuItem className={styles.menuItem}>Component 2</MenuItem>
+        </SubMenu>
+        <SubMenu label="Vue.js">
           <MenuItem className={styles.menuItem}>Component 1</MenuItem>
           <MenuItem className={styles.menuItem}>Component 2</MenuItem>
         </SubMenu>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,41 @@
-import Header from "@/components/Header";
-import SideMenu from "@/components/common/SideMenu";
-const Blog = () => {
+import Header from '@/components/Header';
+import SideMenu from '@/components/common/SideMenu';
+
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
+
+import styles from '@/styles/Blog.module.css';
+const Blog = ({ source }) => {
   return (
     <div>
       <Header />
       <div>
         <h1>Blog</h1>
-        <SideMenu />
+        <div className={styles.blog}>
+          <SideMenu />
+          <div className="wrapper">
+            <div className={styles.content}>
+              <MDXRemote {...source} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const source = `# My MDX page
+
+  This is a list in markdown:
+  
+
+  - One
+  - Two
+  - Three`;
+
+  const mdxSource = await serialize(source);
+  return { props: { source: mdxSource } };
+}
 
 export default Blog;
